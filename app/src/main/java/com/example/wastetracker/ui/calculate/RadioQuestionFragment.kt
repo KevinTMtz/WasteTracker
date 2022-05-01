672515e652e47438bd201e0a12f39f5ad161f6d0
Sebastián Rodríguez Galarza <sebastian_rodriguezg@yahoo.com>
2022-05-01 13:20:44 -0500

    Calculate tab UI (#1)
    * Implement SliderQuestionFragment for CalculateFragment
    
    * Implement RadioQuestionFragment for CalculateFragment
    
    * Implement navigation & add ResultsFragment

---
+++ app/src/main/java/com/example/wastetracker/ui/calculate/RadioQuestionFragment.kt
@@ -0,0 +1,39 @@
+package com.example.wastetracker.ui.calculate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import com.example.wastetracker.R
+import com.example.wastetracker.databinding.FragmentRadioQuestionBinding
+import com.example.wastetracker.databinding.FragmentSliderQuestionBinding
+
+class RadioQuestionFragment(private val number: Int, private val question: String) : Fragment() {
+
+    private lateinit var binding: FragmentRadioQuestionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRadioQuestionBinding.inflate(inflater, container, false)
+
+        binding.questionNumber.text = "Question #${number}"
+        binding.questionText.text = question
+        // TODO: Add radio buttons programmatically
+        binding.radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            val checked = group.findViewById<RadioButton>(checkedId)
+            // TODO: Find a way to have a value and text properties for radio button
+        }
+
+        return binding.root
+    }
+
+}
