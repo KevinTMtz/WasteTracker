d2329cda79df985c285c6982e3b572e962be73d0
Jose Tlacuilo <65783495+tlacuilose@users.noreply.github.com>
2022-05-26 17:28:32 -0500

    Locale option for identify waste API (#15)
    * Adds locale options to model results.
    
    * Update links in spanish and fixes other fields.

--- ML/wasteid/waste.py
+++ ML/wasteid/waste.py
@@ -1,4 +1,4 @@
-CLASSES = [
+ENG = [
     {
         "name": "Aluminium",
         "description": "In 2018, 3.9 million tons of aluminum municipal solid waste was generated. The total recycling rate for aluminum items was 34.9 percent. Aluminium is one of the most desirable metals in the world and is used in many industries such as transport, food, electronics and electrical power transmission and construction. It is also very popular to recycle because of its low energy needs and high scrap value.",
@@ -45,3 +45,54 @@ CLASSES = [
         "howto": "https://www.slrecyclingltd.co.uk/benefits-of-recycling-wood/"
     },
 ]
+
+ESP = [
+    {
+        "name": "Aluminio",
+        "description": "En 2018, se generaron 3,9 millones de toneladas de residuos sólidos municipales de aluminio. La tasa total de reciclaje de artículos de aluminio fue del 34,9%. El aluminio es uno de los metales más deseados en el mundo y se utiliza en muchas industrias como el transporte, la alimentación, la electrónica y la transmisión de energía eléctrica y la construcción. También es muy popular para reciclar por su bajo consumo energético y su alto valor como chatarra",
+        "howto": "https://climate.selectra.com/es/reciclaje/aluminio"
+    },
+    {
+        "name": "Cartón",
+        "description": "El cartón, también denominado cartón ondulado, es un material reciclable que es reciclado por empresas de pequeña y gran escala para ahorrar dinero en los costes de eliminación de residuos. El reciclaje de cartón consiste en el reprocesamiento y la reutilización de hojas gruesas o papeles rígidos de varias capas que han sido utilizados, desechados o considerados como residuos. Las cajas de cartón suelen ser hojas de papel gruesas o resistentes, conocidas por su durabilidad y dureza. Algunos ejemplos de cartón son las cajas de embalaje, los cartones de huevos, las cajas de zapatos y las cajas de cereales",
+        "howto": "https://climate.selectra.com/es/reciclaje/carton"
+    },
+    {
+        "name": "Vidrio",
+        "description": "El vidrio, especialmente los envases de vidrio para alimentos y bebidas, puede reciclarse una y otra vez. En Estados Unidos, en 2018, se generaron 12,3 millones de toneladas de vidrio, de las cuales el 31,3% se recicló. Fabricar vidrio nuevo a partir de vidrio reciclado suele ser más barato que utilizar materias primas.",
+        "howto": "https://climate.selectra.com/es/reciclaje/vidrio"
+    },
+    {
+        "name": "Residuos orgánicos",
+        "description": "La EPA estima que en 2018 se generaron 63,1 millones de toneladas de residuos de alimentos en los sectores comercial, institucional y residencial. Los alimentos no se pueden reciclar. Sin embargo, tirar la comida a la basura no es la mejor opción de gestión. Los alimentos que acaban en los vertederos provocan emisiones de metano que contribuyen al cambio climático. La EPA recomienda reducir el desperdicio de alimentos comprando solo lo que se necesita y consumiendo las sobras. El compostaje es un método de eliminación de residuos alimentarios respetuoso con el medio ambiente. En 2018, alrededor del 4,1 por ciento de los residuos de alimentos se compostaron",
+        "howto": "https://www.crehana.com/blog/trabajo-remoto/como-hacer-una-composta-casera/"
+    },
+    {
+        "name": "Otros plásticos",
+        "description": "En 2018 se generaron más de 35 millones de toneladas de plásticos en Estados Unidos y solo se recicló el 8,7%. Algunos tipos de plásticos no se aceptan en los programas de reciclaje de la comunidad. Consulta con tu programa de reciclaje local para saber qué tipos de plástico aceptan. Cuando sea posible, compre productos hechos de materiales plásticos reciclados.",
+        "howto": "https://climate.selectra.com/es/reciclaje/plastico"
+    },
+    {
+        "name": "Papel y cartón",
+        "description": "El papel constituye el 23 por ciento de los residuos sólidos municipales (basura) generados cada año, más que cualquier otro material. Los estadounidenses reciclaron alrededor del 68% del papel que utilizaron en 2018. Este papel recuperado se utiliza para fabricar nuevos productos de papel, lo que salva árboles y otros recursos naturales. La mayoría de los programas de reciclaje de comunidades u oficinas aceptan papel y productos de papel. Comprueba qué programa de tu comunidad u oficina acepta antes de depositarlo en el contenedor. Busque productos fabricados con papel reciclado cuando haga sus compras. Mejor aún, considera si realmente necesitas imprimir en primer lugar",
+        "howto": "https://climate.selectra.com/es/reciclaje/papel"
+    },
+    {
+        "name": "Plástico",
+        "description": "En 2018 se generaron más de 35 millones de toneladas de plásticos en Estados Unidos y solo se recicló el 8,7%. Algunos tipos de plásticos no se aceptan en los programas de reciclaje de la comunidad. Consulta con tu programa de reciclaje local para saber qué tipos de plástico aceptan. Cuando sea posible, compre productos hechos de materiales plásticos reciclados.",
+        "howto": "https://climate.selectra.com/es/reciclaje/plastico"
+    },
+    {
+        "name": "Textiles",
+        "description": "La moda rápida, cuando las marcas de ropa renuevan sus colecciones con mayor frecuencia, nos incita a llenar nuestros armarios de ropa que nunca usamos, o que usamos muy poco, y de la que luego nos deshacemos. Además de cuestionar nuestros hábitos de compra de ropa, el reciclaje parece ser una respuesta para contrarrestar los millones de toneladas de gases de efecto invernadero que emite la industria textil cada año",
+        "howto": "https://climate.selectra.com/es/reciclaje/ropa"
+    },
+    {
+        "name": "Madera",
+        "description": "Las fuentes de madera en los residuos sólidos urbanos (RSU) incluyen muebles, otros bienes duraderos (por ejemplo, armarios para equipos electrónicos), embalajes de madera (cajas, palés) y algunos otros productos diversos. Los residuos de madera son un recurso precioso que está disponible en abundancia y que puede utilizarse para el reciclaje de materiales o la producción de energía, según el grado de calidad",
+        "howto": "https://www.cleanipedia.com/ar/sustentabilidad/reciclaje-de-madera.html"
+    },
+]
+
+CLASSES = { "en": ENG, "es": ESP}
+
